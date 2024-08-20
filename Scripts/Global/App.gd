48209e9d2c1360656b54b55c7d3a2a0c2bb04954
Atomic Junky <67459553+atomic-junky@users.noreply.github.com>
2024-08-20 20:00:15 +0200

    Update window when resizing

--- Scripts/Global/App.gd
+++ Scripts/Global/App.gd
@@ -6,12 +6,19 @@ const BASE_SCALE_DPI = 92.56
 
 
 func _ready() -> void:
+	update_window(true)
+	get_window().connect("size_changed", update_window)
+
+
+func update_window(update_size: bool = false):
 	var screen_dpi = DisplayServer.screen_get_dpi()
 	var screen_size = DisplayServer.window_get_size()
 	# Guess the screen scale because [DisplayServer.screen_get_scale()] only work on macOS and Linux.
 	var guessed_screen_scale: float = min(round(screen_dpi/BASE_SCALE_DPI), 2.0)
 	
-	DisplayServer.window_set_size(screen_size*guessed_screen_scale)
 	ProjectSettings.set_setting("display/window/stretch/scale", guessed_screen_scale)
 	ProjectSettings.save_custom("user://override.cfg")
-	get_window().move_to_center()
+	
+	if update_size:
+		DisplayServer.window_set_size(screen_size*guessed_screen_scale)
+		get_window().move_to_center()
