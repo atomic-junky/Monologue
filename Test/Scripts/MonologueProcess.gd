d85abea5565fb1fb403f69729906cbbdd45e093c
Lemoney <railkill@gmail.com>
2024-10-01 20:33:00 +0800

    Allow backwards compatibility in MonologueProcess

--- Test/Scripts/MonologueProcess.gd
+++ Test/Scripts/MonologueProcess.gd
@@ -62,13 +62,15 @@ func next() -> void:
 
 func parse_events() -> void:
 	for event in events:
-		var variable = get_variable(event.get("Variable"))
+		var condition = event.get("Condition")  # v2.x compatibility
+		var c_dict = condition if condition else event
+		var variable = get_variable(c_dict.get("Variable"))
 		if not variable:
 			continue
 		var v_value = variable.get("Value")
-		var c_value = event.get("Value")
+		var c_value = c_dict.get("Value")
 		var condition_pass: bool
-		match event.get("Operator"):
+		match c_dict.get("Operator"):
 			"==": condition_pass = v_value == c_value
 			">=": condition_pass = v_value >= c_value
 			"<=": condition_pass = v_value <= c_value
@@ -127,7 +129,8 @@ func process_node(node: Dictionary) -> void:
 					monologue_notify.emit(NotificationLevel.WARN,
 							"Can't find option with id: %s" % option_id)
 					continue
-				if option.get("EnableByDefault") == false:
+				if option.get("Enable") == false or \
+						option.get("EnableByDefault") == false:
 					continue
 				options.append(option)
 			monologue_new_choice.emit(options)
@@ -144,61 +147,22 @@ func process_node(node: Dictionary) -> void:
 			process_setter(node)
 			next()
 		
+		"NodeAction":
+			next_id = node.get("NextID")
+			process_action(node.get("Action"))
+		
 		"NodeAudio":
 			next_id = node.get("NextID")
-			var path = node.get("Audio")
-			if path.is_relative_path():
-				path = base_path + "/" + path
-				if not FileAccess.file_exists(path):
-					var backup = "/assets/audios/"
-					path = base_path + backup + path.get_file()
-			
-			var volume = node.get("Volume")
-			var pitch = node.get("Pitch")
-			var loop = node.get("Loop")
-			var track = play_audio(path, loop, volume, pitch)
-			if track:
-				monologue_play_audio.emit(path, track)
+			process_audio(node)
 			next()
 		
 		"NodeBackground":
 			next_id = node.get("NextID")
-			var bg = Image.new()
-			var path = node.get("Image")
-			if path.is_relative_path():
-				path = base_path + "/" + path
-				if not FileAccess.file_exists(path):
-					var backup = "/assets/backgrounds/"
-					path = base_path + backup + path.get_file()
-
-			if FileAccess.file_exists(path):
-				var status = bg.load(path)
-				if status == OK:
-					var texture = ImageTexture.create_from_image(bg)
-					monologue_update_background.emit(path, texture)
-				else:
-					monologue_notify.emit(NotificationLevel.WARN,
-							"Failed to load background (%s)" % path)
+			process_background(node)
 			next()
 		
 		"NodeCondition":
-			var variable = get_variable(node.get("Variable"))
-			if variable:
-				var if_nid = node.get("IfNextID")
-				var else_nid = node.get("ElseNextID")
-				var v_val = variable.get("Value")
-				var c_val = node.get("Value")
-				next_id = if_nid
-				match node.get("Operator"):
-					"==": next_id = if_nid if v_val == c_val else else_nid
-					">=": next_id = if_nid if v_val >= c_val else else_nid
-					"<=": next_id = if_nid if v_val <= c_val else else_nid
-					"!=": next_id = if_nid if v_val != c_val else else_nid
-			else:
-				var var_name = variable.get("Name")
-				var warning = "Can't find variable %s. Skipping." % var_name
-				monologue_notify.emit(NotificationLevel.WARN, warning)
-			next()
+			process_condition(node)
 		
 		"NodeWait":
 			next_id = node.get("NextID")
@@ -207,6 +171,88 @@ func process_node(node: Dictionary) -> void:
 		"NodeEndPath":
 			monologue_end.emit(node)
 			next_story(node.get("NextStory"))
+
+
+## @deprecated: v3.x no longer uses NodeAction in this way
+func process_action(raw_action: Dictionary) -> void:
+	match raw_action.get("$type"):
+		"ActionOption":
+			raw_action["SetType"] = "Option"
+			process_setter(raw_action)
+		
+		"ActionVariable":
+			raw_action["SetType"] = "Variable"
+			process_setter(raw_action)
+		
+		"ActionCustom":
+			match raw_action.get("CustomType"):
+				"PlayAudio": process_audio(raw_action)
+				"UpdateBackground": process_background(raw_action)
+				"Other": monologue_custom_action.emit(raw_action)
+		
+		"ActionTimer":
+			start_timer(raw_action.get("Value", 0.0))
+			return  # return so next() is not called immediately
+	next()
+
+
+func process_audio(raw_audio: Dictionary) -> void:
+	next_id = raw_audio.get("NextID")
+	var path = raw_audio.get("Audio")
+	if path.is_relative_path():
+		path = base_path + "/" + path
+		if not FileAccess.file_exists(path):
+			var backup = "/assets/audios/"
+			path = base_path + backup + path.get_file()
+	
+	var volume = raw_audio.get("Volume")
+	var pitch = raw_audio.get("Pitch")
+	var loop = raw_audio.get("Loop")
+	var track = play_audio(path, loop, volume, pitch)
+	if track:
+		monologue_play_audio.emit(path, track)
+
+
+func process_background(raw_background: Dictionary) -> void:
+	var bg = Image.new()
+	var path = raw_background.get("Image")
+	if path.is_relative_path():
+		path = base_path + "/" + path
+		if not FileAccess.file_exists(path):
+			var backup = "/assets/backgrounds/"
+			path = base_path + backup + path.get_file()
+
+	if FileAccess.file_exists(path):
+		var status = bg.load(path)
+		if status == OK:
+			var texture = ImageTexture.create_from_image(bg)
+			monologue_update_background.emit(path, texture)
+		else:
+			monologue_notify.emit(NotificationLevel.WARN,
+					"Failed to load background (%s)" % path)
+
+
+func process_condition(raw_condition: Dictionary) -> void:
+	var condition = raw_condition.get("Condition")  # v2.x compatibility
+	var c_dict = condition if condition else raw_condition
+	var variable = get_variable(c_dict.get("Variable"))
+	
+	if variable:
+		var if_nid = raw_condition.get("IfNextID")
+		var else_nid = raw_condition.get("ElseNextID")
+		var v_val = variable.get("Value")
+		var c_val = c_dict.get("Value")
+		next_id = if_nid
+		match c_dict.get("Operator"):
+			"==": next_id = if_nid if v_val == c_val else else_nid
+			">=": next_id = if_nid if v_val >= c_val else else_nid
+			"<=": next_id = if_nid if v_val <= c_val else else_nid
+			"!=": next_id = if_nid if v_val != c_val else else_nid
+	else:
+		var var_name = variable.get("Name")
+		var warning = "Can't find variable %s. Skipping." % var_name
+		monologue_notify.emit(NotificationLevel.WARN, warning)
+	next()
 
 
 func process_setter(raw_setter: Dictionary) -> void:
@@ -234,10 +280,6 @@ func process_setter(raw_setter: Dictionary) -> void:
 			else:
 				var warning = "Can't find variable. Skipping."
 				monologue_notify.emit(NotificationLevel.WARN, warning)
-		
-		"ActionCustom":
-			# TODO: Move out to NodeAction
-			monologue_custom_action.emit(raw_setter)
 
 
 func process_conditional_text(text: String) -> String:
@@ -393,9 +435,10 @@ func set_option_value(option_id: String, value: bool) -> void:
 func select_option(raw_option: Dictionary) -> void:
 	monologue_option_chosen.emit(raw_option)
 	
-	if raw_option.keys().has("OneShot") and raw_option.keys().has("EnableByDefault"):
+	if raw_option.keys().has("OneShot"):
 		next_id = raw_option.get("NextID", -1)
 		if raw_option.get("OneShot") == true:
+			raw_option["Enable"] = false  # v2.x compatibility
 			raw_option["EnableByDefault"] = false
 		next()
 	else:
