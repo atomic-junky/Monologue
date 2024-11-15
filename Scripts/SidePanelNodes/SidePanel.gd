bc96a46e50b3f2057b8e626f910cb0afb719cb5e
Atomic Junky <67459553+atomic-junky@users.noreply.github.com>
2024-11-15 22:47:10 +0100

    Fix theme and simplify field loading

--- Scripts/SidePanelNodes/SidePanel.gd
+++ Scripts/SidePanelNodes/SidePanel.gd
@@ -41,11 +41,11 @@ func on_graph_node_selected(node: MonologueGraphNode, bypass: bool = false):
 	
 	if not node.is_editable():
 		return
-		
 	
 	var items = node._get_field_groups()
 	var already_invoke := []
-	
+	var property_names = node.get_property_names()
+
 	for item in items:
 		if item is String:
 			var field = node.get(item).show(fields_container)
@@ -54,32 +54,28 @@ func on_graph_node_selected(node: MonologueGraphNode, bypass: bool = false):
 		else:
 			for group in item:
 				var fields = item[group]
-				var cf: CollapsibleField = collapsible_field.instantiate()
-				fields_container.add_child(cf)
-				cf.set_title(group)
+				var field_obj: CollapsibleField = collapsible_field.instantiate()
+				fields_container.add_child(field_obj)
+				field_obj.set_title(group)
 				
 				for field_name in fields:
-					for property_name in node.get_property_names():
-						if field_name != property_name:
-							continue
-						
-						var field = node.get(property_name).show(fields_container)
-						field.set_label_text(property_name.capitalize())
-						
-						fields_container.remove_child(field)
-						cf.add_item(field)
-						already_invoke.append(property_name)
-	
-	for property_name in node.get_property_names():
+					var field = node.get(field_name).show(fields_container)
+					field.set_label_text(field_name.capitalize())
+
+					fields_container.remove_child(field)
+					field_obj.add_item(field)
+					already_invoke.append(field_name)
+
+	for property_name in property_names:
 		if property_name in already_invoke:
 			continue
-		
+
 		if property_name == "id":
 			id_field = node.get(property_name).show(topbox, 0)
 		else:
 			var field = node.get(property_name).show(fields_container)
 			field.set_label_text(property_name.capitalize())
-	
+
 	show()
 
 
