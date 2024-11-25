a63a77e0573eda6dd6766714ee14cd9e5d67a64a
Lemoney <railkill@gmail.com>
2024-11-25 20:57:55 +0800

    Fix unit test

--- Test/Unit/TestGraphNodeSelector.gd
+++ Test/Unit/TestGraphNodeSelector.gd
@@ -1,44 +1,25 @@
 extends GdUnitTestSuite
 
 
-var buttons = [
-	["SentenceSelector"], ["ChoiceSelector"], ["RandomSelector"], 
-	["BridgeSelector"], ["ConditionSelector"], 
-	["ActionSelector"], ["AudioSelector"],
-	["BackgroundSelector"], ["WaitSelector"],
-	["SetterSelector"], ["RerouteSelector"], ["EndPathSelector"]
-]
-
-
-@warning_ignore("unused_parameter")
-func test_pressed_signal_connected(node: String, test_parameters = buttons):
-	var s = scene_runner("res://Objects/SubComponents/GraphNodeSelector.tscn")
-	var button = s.find_child(node)
-	var connection = button.get_signal_connection_list("pressed")[0]
-	var signature = str(connection.get("signal"))
-	var callback = str(connection.get("callable"))
-	assert_str(signature).ends_with("[signal]pressed")
-	assert_str(callback).ends_with("_on_selector_btn_pressed")
-
-
 func test_enable_picker_mode():
 	var from = "Omelette du Fromage"
 	var port = 4
 	var release = Vector2(-100.05, 100.09)
 	var center = Vector2(123.456, -123.456)
 	
-	var selector = auto_free(GraphNodeSelector.new())
-	selector.visible = false
-	selector.enable_picker_mode(from, port, release, center)
-	assert_str(selector.node).is_equal(from)
-	assert_int(selector.port).is_equal(port)
-	assert_vector(selector.location).is_equal(center)
-	assert_vector(selector.position).is_equal(Vector2i(-150, 100))
-	assert_bool(selector.visible).is_true()
+	var runner = scene_runner("res://Objects/UI/GraphNodePicker.tscn")
+	runner.invoke("set_visible", false)
+	runner.invoke("_on_enable_picker_mode", from, port, release, null, center)
+	
+	assert_str(runner.get_property("from_node")).is_equal(from)
+	assert_int(runner.get_property("from_port")).is_equal(port)
+	assert_vector(runner.get_property("center")).is_equal(center)
+	assert_vector(runner.get_property("release")).is_equal(release)
+	assert_bool(runner.get_property("visible")).is_true()
 
 
-func test_disable_picker_mode():
-	var selector = auto_free(GraphNodeSelector.new())
+func test_close():
+	var selector = auto_free(GraphNodePicker.new())
 	selector.visible = true
-	selector.disable_picker_mode()
+	selector.close()
 	assert_bool(selector.visible).is_false()
